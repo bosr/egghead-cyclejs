9dc254dc3837a0de02d7ece7e69387f547c8d48f
Romain Bossart <romain.bossart@fastmail.com>
2017-09-12 22:28:40 +0200

    2. organize code in the main and effect functions

--- src/code.js
+++ src/code.js
@@ -1,17 +1,29 @@
 import xs from 'xstream';
 
-// Logic
-xs.periodic(1000)             // !--!--!--!--!--
-  .fold(prev => prev + 1, 0)  // 0--1--2--3--4--
-  .map(i => `seconds elapsed: ${i}`)
+function main() {
+  return xs.periodic(1000)      // !--!--!--!--!--
+    .fold(prev => prev + 1, 0)  // 0--1--2--3--4--
+    .map(i => `seconds elapsed: ${i}`);
+}
 
 // need to separate logic from effects
 
-// Effects
-  .subscribe({
+function domDriver(text$) {
+  text$.subscribe({
     next: str => {
-      console.log(str);
-      // const elem = document.querySelector('#app');
-      // elem.textContent = str;
+      const elem = document.querySelector('#app');
+      elem.textContent = str;
     }
   });
+}
+
+function logDriver(msg$) {
+  msg$.subscribe({
+    next: str => {
+      console.log(str);
+    }
+  });
+}
+
+const sink = main();
+logDriver(sink);
